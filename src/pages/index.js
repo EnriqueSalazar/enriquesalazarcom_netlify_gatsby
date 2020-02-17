9b2fb492e7f12bffd1950b4924cdc41c422b9f43
Enrique Salazar <enrique.salazar@gmail.com>
2020-02-17 19:08:25 +0100

    initial layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,49 @@
 import React from 'react';
 import { Link } from 'gatsby';
+// import { Button } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 
-import Layout from '../components/layout';
-import Image from '../components/image';
-import SEO from '../components/seo';
+// import Layout from '../components/layout';
+// import Image from '../components/image';
+// import SEO from '../components/seo';
 
 const IndexPage = () => (
-    <Layout>
-        <SEO title="Home" />
-        <h1>Hi people</h1>
-        <p>Welcome to your new CI test.</p>
-        <p>Now go build something great.</p>
-        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-            <Image />
-        </div>
-        <Link to="/page-2/">Go to page 2</Link>
-    </Layout>
+    <Container maxWidth="sm">
+        <Grid container>
+            <Grid item xs={12}>
+                <div
+                    style={{
+                        color: 'hotpink',
+                        backgroundColor: 'green',
+                    }}
+                >
+                    Enrique Salazar Sebastiani
+                </div>
+            </Grid>
+            <Grid item xs={12}>
+                <div
+                    style={{
+                        color: 'hotpink',
+                        backgroundColor: 'green',
+                    }}
+                >
+                    Developer
+                </div>
+            </Grid>
+            <Grid item xs={6}>
+                <div>LinkedIn</div>
+            </Grid>
+            <Grid item xs={6}>
+                <div>Github</div>
+            </Grid>
+            {/* <Button variant="contained" color="secondary">
+                Secondary
+            </Button>
+            <Link to="/page-2/">Go to page 2</Link> */}
+        </Grid>
+    </Container>
 );
 
 export default IndexPage;
