edea67e96cd44997be70bda3025cd456a3afd1aa
Enrique Salazar <enrique.salazar@gmail.com>
2020-02-17 22:06:26 +0100

    fixed layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,50 @@
 import React from 'react';
 // import { Link } from 'gatsby';
-// import { Button } from '@material-ui/core';
-// import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import './global.css';
 
-// import Layout from '../components/layout';
-// import Image from '../components/image';
 // import SEO from '../components/seo';
 
 const IndexPage = () => (
-    <Container maxWidth="sm">
-        <Grid container>
-            <Grid item xs={12}>
-                <div
-                    style={{
-                        color: 'hotpink',
-                        backgroundColor: 'green',
-                    }}
-                >
-                    Enrique Salazar Sebastiani
-                </div>
-            </Grid>
-            <Grid item xs={12}>
-                <div
-                    style={{
-                        color: 'hotpink',
-                        backgroundColor: 'green',
-                    }}
-                >
-                    Developer
-                </div>
-            </Grid>
-            <Grid item xs={6}>
-                <div>LinkedIn</div>
-            </Grid>
-            <Grid item xs={6}>
-                <div>Github</div>
-            </Grid>
-            {/* <Button variant="contained" color="secondary">
+    <React.Fragment>
+        <Container maxWidth="sm" 
+        style={{ height: '100%' }}
+        >
+            <CssBaseline />
+            <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                style={{ height: '100%' }}
+            >
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Typography variant="h4" align="center">
+                            Enrique Salazar Sebastiani
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant="h6" align="center">Developer</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="h6" align="left">LinkedIn</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="h6" align="right">Github</Typography>
+                    </Grid>
+                </Grid>
+
+                {/* <Button variant="contained" color="secondary">
                 Secondary
             </Button>
             <Link to="/page-2/">Go to page 2</Link> */}
-        </Grid>
-    </Container>
+            </Grid>
+        </Container>
+    </React.Fragment>
 );
 
 export default IndexPage;
